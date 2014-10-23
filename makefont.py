5672c58c43533c3e1fb37890f277cc300d63de73
Ryan Dorsey <ryanjdorsey@gmail.com>
2014-10-23 18:55:20 -0400

    Removed unnecessary space glyph.  makefont.py can now guess an em width.

--- makefont.py
+++ makefont.py
@@ -10,7 +10,7 @@
 
 import fontforge, psMat
 import unicodedata
-import os
+import os, sys
 import os.path as path
 
 # Colors for printing
@@ -31,64 +31,95 @@ SPACES = (('space',                 0.4),
           ('em quad',               1.0),
           ('en space',              0.5),
           ('em space',              1.0),
+          ('three-per-em space',    0.3333),
+          ('four-per-em space',     0.25),
+          ('six-per-em space',      0.1667),
+          # ('figure space', )
+          # ('punctuation space', )
           ('thin space',            0.2),
           ('hair space',            0.1),
           ('zero width space',      0.0),
-          # Not a space, but might as well go here
+          ('zero width non-joiner', 0.0),
+          ('zero width joiner',     0.0),
+          # Not spaces, but might as well go here
+          ('left-to-right mark',    0.0),
           ('right-to-left mark',    0.0),
 )
 
-font = fontforge.font()
-# Some basic metadata.
-font.encoding    = 'UnicodeBmp'
-font.copyright   = ('Copyright (c) 2014 Ryan Dorsey\n'
-                    'Licensed under SIL Open Font License v1.1 (http://scripts.sil.org/OFL)\n'
-                    'Created with FontForge 2.0 (http://fontforge.sf.net)')
-font.familyname  = 'Mazon Hebrew'
-font.fontname    = 'MazonHebrew-Regular'
-font.fullname    = 'Mazon Hebrew Regular'
-font.version     = 'v0.1'
-font.weight      = 'Regular'
-font.descent     = 247.0
-font.ascent      = 1000.0 - 247.0
+# Unfortunately, this can only be done with a font that is *already* available.
+# So if you need to use it, run generate, then find the em width, then generate,
+# etc.
+def find_em_width():
+    font = fontforge.open('MazonHebrew-Regular.sfd')
+    # These are the "standard-width" characters in Hebrew.  The rest are
+    # much narrower, excepting shin, which is a bit wider.
+    em_width_chars = u'אבדהחטכךלמםסעפףצקרת'
+    total = 0
+    for c in em_width_chars:
+        total += font[ord(c)].width
+    font.close()
+    return float(total) / len(em_width_chars)
 
-# This works, but prints out "failed to parse color" 6 times per glyph.
-# That is going to be annoying as heck unless I can suppress that output.
-#
-# TODO: Needs to handle `space` specially.
-for d in DIRS:
-    for f in os.listdir(d):
-        fullpath = path.join(d, f)
-        if path.isfile(fullpath):
-            print 'Processing file: {}'.format(f)
-            # Retrieve the filename sans extension, i.e., the glyph's unicode
-            # name.
-            glyphname = path.splitext(path.basename(f))[0]
-            try:
-                glyphnum = ord(unicodedata.lookup(glyphname))
-            except KeyError:
-                print (TERMCOLORRED
-                       + 'Filename `{}` does not correspond to a unicode name'
-                       + TERMRESET).format(fullpath)
-                continue
-            glyph = font.createChar(glyphnum)
-            glyph.importOutlines(fullpath)
-            glyph.correctDirection()
-            glyph.left_side_bearing  = 60
-            glyph.right_side_bearing = 60
+def generate():
+    font = fontforge.font()
+    # Some basic metadata.
+    font.copyright   = ('Copyright (c) 2014 Ryan Dorsey\n'
+                        'Licensed under SIL Open Font License v1.1 (http://scripts.sil.org/OFL)\n'
+                        'Created with FontForge 2.0 (http://fontforge.sf.net)')
+    font.encoding    = 'UnicodeBmp'
+    font.descent     = 247.0
+    font.ascent      = 1000.0 - font.descent # calculate after font.descent
+    font.familyname  = 'Mazon Hebrew'
+    font.fontname    = 'MazonHebrew-Regular'
+    font.fullname    = 'Mazon Hebrew Regular'
+    font.version     = 'v0.1'
+    font.weight      = 'Regular'
 
-# Make whitespace characters.
-for (spacechar, spacewidth) in SPACES:
-    print 'Creating space: {}'.format(spacechar)
-    try:
-        glyphnum = ord(unicodedata.lookup(spacechar))
-    except KeyError:
-        print (TERMCOLORRED
-               + '`{}` in SPACES does not correspond to a unicode name'
-               + TERMRESET).format(spacechar)
-    glyph = font.createChar(glyphnum)
-    glyph.width = int(round(spacewidth * EMWIDTH))
-    
+    # This works, but prints out "failed to parse color" 6 times per glyph.
+    # That is going to be annoying as heck unless I can suppress that output.
+    #
+    # TODO: Needs to handle `space` specially.
+    for d in DIRS:
+        for f in os.listdir(d):
+            fullpath = path.join(d, f)
+            if path.isfile(fullpath):
+                print 'Processing file: {}'.format(f)
+                # Retrieve the filename sans extension, i.e., the glyph's
+                # unicode name.
+                glyphname = path.splitext(path.basename(f))[0]
+                try:
+                    glyphnum = ord(unicodedata.lookup(glyphname))
+                except KeyError:
+                    print (TERMCOLORRED
+                           + 'Filename `{}` does not correspond to a unicode name'
+                           + TERMRESET).format(fullpath)
+                    continue
+                glyph = font.createChar(glyphnum)
+                glyph.importOutlines(fullpath)
+                glyph.correctDirection()
+                glyph.left_side_bearing  = 60
+                glyph.right_side_bearing = 60
 
-font.save('MazonHebrew-Regular.gen.sfd')
-font.generate('MazonHebrew-Regular.gen.otf')
+    # Make whitespace characters.
+    for (spacechar, spacewidth) in SPACES:
+        print 'Creating space: {}'.format(spacechar)
+        try:
+            glyphnum = ord(unicodedata.lookup(spacechar))
+        except KeyError:
+            print (TERMCOLORRED
+                   + '`{}` in SPACES does not correspond to a unicode name'
+                   + TERMRESET).format(spacechar)
+            continue
+        glyph = font.createChar(glyphnum)
+        glyph.width = int(round(spacewidth * EMWIDTH))
+
+    font.save('MazonHebrew-Regular.gen.sfd')
+    font.generate('MazonHebrew-Regular.gen.otf')
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        generate()
+
+    else:
+        if sys.argv[1] == '--find-em-width' or sys.argv[1] == '-m':
+            print 'The average em-width is: ', find_em_width()
