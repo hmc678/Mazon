c7ade40d09195e3300e44fbac889ad467556caa7
Ryan Dorsey <ryanjdorsey@gmail.com>
2014-10-26 14:55:17 -0400

    Began modernizing and cleaning the code.
    Added __future__ imports, added a real argument parser, added an option to
    download a local copy of toml if the user doesn't have it, and broke some
    components out into separate functions.

--- makefont.py
+++ makefont.py
@@ -8,26 +8,44 @@
 # This license is copied below, and is also available with a FAQ at:
 # http://scripts.sil.org/OFL
 
-import fontforge, psMat
-import unicodedata
-import os, sys
+from __future__ import division, print_function
+import argparse
+import os
 import os.path as path
+import subprocess as sub
+import sys
+import unicodedata
 
-# TODO: make this less fatal and more helpful.  Maybe have a local version?
-try:
-    import toml
-except ImportError:
-    print 'Please install toml, perhaps via `pip install toml`?'
-    exit(1)
+import fontforge
+import psMat
 
-config = toml.load('config.toml')
+if sys.version_info.major < 3:
+    input = raw_input
 
-# Should these be in the config file?  I dunno....
-config['term color red'] = '\033[31;1m'
-config['term reset'] = '\033[0m'
-# These are the "standard-width" characters in Hebrew.  The rest are
-# much narrower, excepting shin, which is a bit wider.
-config['em width chars'] = u'אבדהחטכךלמםסעפףצקרת'
+config = None
+
+def download_local_toml():
+    TOMLVER = '0.8.2'
+
+    if os.path.exists('toml-' + TOMLVER) or os.path.exists('toml.py'):
+        printerr('Exiting, since toml files already exist in the current '
+                 'directory')
+        sys.exit(1)
+
+    try:
+        if sys.platform.startswith('win32'):
+            raise Exception('Windows support not yet included.  Sorry!')
+        else:
+            wget = sub.Popen(['wget', '-O', '-',
+                              'https://pypi.python.org/packages/source/t/toml/toml-' + TOMLVER + '.tar.gz'],
+                             stdout=sub.PIPE)
+            sub.check_call(['tar', '-xz'], stdin=wget.stdout)
+            wget.wait()
+            sub.check_call(['mv', 'toml-' + TOMLVER + '/toml.py', 'toml.py'])
+            sub.check_call(['rm', '-r', 'toml-' + TOMLVER])
+    except Exception as e:
+        printerr('Local toml install failed: {}'.format(e))
+        sys.exit(1)
 
 # Unfortunately, this can only be done with a font that is *already* available.
 # So if you need to use it, run generate, then find the em width, then generate,
@@ -44,7 +62,8 @@ def generate():
     font = fontforge.font()
     # Some basic metadata.
     font.copyright   = ('Copyright (c) 2014 Ryan Dorsey\n'
-                        'Licensed under SIL Open Font License v1.1 (http://scripts.sil.org/OFL)\n'
+                        'Licensed under SIL Open Font License v1.1 '
+                        '(http://scripts.sil.org/OFL)\n'
                         'Created with FontForge 2.0 (http://fontforge.sf.net)')
     font.encoding    = config['specs']['encoding']
     font.descent     = config['specs']['descent']
@@ -61,16 +80,16 @@ def generate():
         for f in os.listdir(d):
             fullpath = path.join(d, f)
             if path.isfile(fullpath):
-                print 'Processing file: {}'.format(f)
+                print('Processing file: {}'.format(f))
                 # Retrieve the filename sans extension, i.e., the glyph's
                 # unicode name.
                 glyphname = path.splitext(path.basename(f))[0]
                 try:
                     glyphnum = ord(unicodedata.lookup(glyphname))
                 except KeyError:
-                    print (config['term color red']
-                           + 'Filename `{}` does not correspond to a unicode name'
-                           + config['term reset']).format(fullpath)
+                    printerr(('Filename `{}` does not correspond to a '
+                              'unicode name').format(fullpath),
+                             level='Warning')
                     continue
                 glyph = font.createChar(glyphnum)
                 glyph.importOutlines(fullpath)
@@ -80,13 +99,12 @@ def generate():
 
     # Make whitespace characters.
     for (spacechar, spacewidth) in config['specs']['spaces'].items():
-        print 'Creating space: {}'.format(spacechar)
+        print('Creating space: {}'.format(spacechar))
         try:
             glyphnum = ord(unicodedata.lookup(spacechar))
         except KeyError:
-            print (config['term color red']
-                   + '`{}` in SPACES does not correspond to a unicode name'
-                   + config['term reset']).format(spacechar)
+            printerr('`{}` in SPACES does not correspond to a unicode name'
+                     .format(spacechar), level='Warning')
             continue
         glyph = font.createChar(glyphnum)
         glyph.width = int(round(spacewidth * config['specs']['real em']))
@@ -94,10 +112,51 @@ def generate():
     font.save('MazonHebrew-Regular.gen.sfd')
     font.generate('MazonHebrew-Regular.gen.otf')
 
+def printerr(errmsg, level='Error'):
+    red = '\033[31;1m'
+    reset = '\033[0m'
+    print(red + level + ': ' + errmsg + reset, file=sys.stderr)
+
 if __name__ == '__main__':
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('-m', '--find-em-width',
+                           help='find the actual em width of the font',
+                           action='store_true')
+    argparser.add_argument('--install-local-package',
+                           help='install a python package to the current directory',
+                           action='append',
+                           dest='packages')
+    args = argparser.parse_args()
+
+    if args.packages:
+        # Currently we only need one non-standard package, but in the future
+        # maybe there will be more?
+        download_local_toml()
+
+    try:
+        import toml
+        config = toml.load('config.toml')
+        # TODO: get this to import properly as unicode from config.toml
+        #
+        # These are the "standard-width" characters in Hebrew.  The rest are
+        # much narrower, excepting shin, which is a bit wider.
+        config['em width chars'] = u'אבדהחטכךלמםסעפףצקרת'
+    except ImportError:
+        print('''\
+You do not have the python module `toml` installed. This is required to read
+the configuration file which describes our font.  To install this package,
+you have three options:
+    1) Run `[sudo] pip install toml`.
+    2) Manually install the toml package.  Maybe your OS's package manager can
+       do this for you, or you can download it from
+       http://pypi.python.org/pypi/toml/0.8.2 or https://github.com/uiri/toml/
+    3) Try to install a copy into this directory by running
+       `makefont.py --install-local-package toml`''')
+        sys.exit(1)
+
     if len(sys.argv) == 1:
         generate()
-
     else:
-        if sys.argv[1] == '--find-em-width' or sys.argv[1] == '-m':
-            print 'The average em-width is: {}'.format(find_em_width())
+        if args.find_em_width:
+            print('The average em-width is: {}'.format(find_em_width()))
